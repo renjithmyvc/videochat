fb47e79493215f7bff4d55ede26ca79e1aa354c3
renjithmyvc <32498238+renjithmyvc@users.noreply.github.com>
2017-12-25 20:09:46 +0530

    process.env.PORT added

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('hello world'))
 
-app.listen(3000, ()=> console.log('example hello world'))
+app.listen(process.env.PORT || 5000, ()=> console.log('example hello world'))
