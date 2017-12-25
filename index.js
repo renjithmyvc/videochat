69b771fb42b939b724c2b40507a8675a8bf5f619
Renjith Ravindran <renjith.agriya@gmail.com>
2017-12-25 20:36:35 +0530

    dynamic page added

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('hello world'))
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.listen(3000, ()=> console.log('example hello world'))
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(process.env.PORT || 5000);
+console.log(process.env.PORT , ' is the magic port');
